2c47ade7ca88af9982d7164f44d43d447cce3200
yash1201 <yashraj.50thecool@gmail.com>
2020-07-22 02:22:59 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/coronanearbycontact/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.coronanearbycontact;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+import 'nearby_interface.dart';
+import 'registration.dart';
+import 'welcome_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        NearbyInterface.id: (context) => NearbyInterface(),
+      },
+    );
+  }
+}
